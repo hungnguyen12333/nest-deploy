d61b5331e46387cb8fc4e8a666bae47df7cccb25
nguyễn phi hùng <89450553+hungnguyen12333@users.noreply.github.com>
2024-08-27 10:25:15 +0700

    asd

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,20 +9,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [],
-        synchronize: false,
-      }),
-      inject: [ConfigService],
-    }),
+   
   ],
   controllers: [AppController],
   providers: [AppService],
